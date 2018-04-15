75e5c803bcf6840c50162ed3b27a2c05637f2411
Kelvin Cortez <kelvcortez@gmail.com>
2018-04-16 09:38:16 +1200

    Inital Commit, Added "WordsController.cs" and "WordsIndexModel.cs"

---
+++ YourDailyWord/YourDailyWord/Controllers/WordsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YourDailyWord.Models;
+
+namespace YourDailyWord.Controllers
+{
+    public class WordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new WordsIndexModel()
+            {
+
+            };
+            return View(model);
+        }
+    }
+}
---
+++ YourDailyWord/YourDailyWord/Models/WordsIndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourDailyWord.Models
+{
+    public class WordsIndexModel
+    {
+        public IEnumerable<WordGallery> WordGallery {get; set;}
+        public String search { get; set; }
+
+    }
+}
